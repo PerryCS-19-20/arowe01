b80cc941a089c36a13ce07fdbb44bd345c5298a3
arowe01 <arowe01@gmail.com>
2016-12-13 10:49:57 -0700

    Update sitemap.js

--- sitemap.js
+++ sitemap.js
@@ -6,9 +6,9 @@ function buildMap(idName) {
     id.appendChild(sitemap);
     
     //list items
-    addListItem("Home", "home", "https://perryma.tk/arowe01", "sitemap");
-    addListItem("Biography", "bio", "https://perryma.tk/arowe01/bio.html", "sitemap");
-    addListItem("Class Projects", "classProjects", null, "sitemap");
+    addListItem("Home", "homeList", "https://perryma.tk/arowe01", "sitemap");
+    addListItem("Biography", "bioList", "https://perryma.tk/arowe01/bio.html", "sitemap");
+    addListItem("Class Projects","classProjects", null, "sitemap");
     
     //makes list under "Class Projects"
     var cPList = document.createElement("ul");
@@ -17,17 +17,17 @@ function buildMap(idName) {
     classProjects.appendChild(cPList);
     
     //list items under "Class Projects"
-    addListItem("Breakout", "breakout", "https://perryma.tk/arowe01/breakout.html", "classProjectsList");
-    addListItem("Gallery", "gallery", "https://perryma.tk/arowe01/popup.html", "classProjectsList");
-    addListItem("Javascript Color", "color", "https://perryma.tk/arowe01/colors.html", "classProjectsList");
-    addListItem("To Do List", "toDo", "https://perryma.tk/arowe01/todolist.html", "classProjectsList");
-    addListItem("Chalkboard", "chalk", "https://perryma.tk/arowe01/chalkboard.html", "classProjectsList");
-    addListItem("Tic-Tac-Toe", "tictactoe", "https://perryma.tk/arowe01/tictactoe.html", "classProjectsList");
-    addListItem("Multiplication", "multi", "https://perryma.tk/arowe01/multiplication.html", "classProjectsList");
-    addListItem("More Multiplication", "multi2", "https://perryma.tk/arowe01/multiplication2.html", "classProjectsList");
-    addListItem("HTML Test", "htmlTest", "https://perryma.tk/arowe01/htmltest.html", "classProjectsList");
-    addListItem("Cone Volume", "coneVol", "https://perryma.tk/arowe01/cone-volume.html", "classProjectsList");
-    addListItem("Calendar", "calendar", "https://perryma.tk/arowe01/calendar.html", "classProjectsList");
+    addListItem("Breakout", "breakoutList", "https://perryma.tk/arowe01/breakout.html", "classProjectsList");
+    addListItem("Gallery", "galleryList", "https://perryma.tk/arowe01/popup.html", "classProjectsList");
+    addListItem("Javascript Color", "javaColorList", "https://perryma.tk/arowe01/colors.html", "classProjectsList");
+    addListItem("To Do List", "toDoListList", "https://perryma.tk/arowe01/todolist.html", "classProjectsList");
+    addListItem("Chalkboard", "chalkboardList", "https://perryma.tk/arowe01/chalkboard.html", "classProjectsList");
+    addListItem("Tic-Tac-Toe", "ticTacToeList", "https://perryma.tk/arowe01/tictactoe.html", "classProjectsList");
+    addListItem("Multiplication", "multiList", "https://perryma.tk/arowe01/multiplication.html", "classProjectsList");
+    addListItem("More Multiplication", "multi2List", "https://perryma.tk/arowe01/multiplication2.html", "classProjectsList");
+    addListItem("HTML Test", "htmlTestList", "https://perryma.tk/arowe01/htmltest.html", "classProjectsList");
+    addListItem("Cone Volume", "coneVolList", "https://perryma.tk/arowe01/cone-volume.html", "classProjectsList");
+    addListItem("Calendar", "calendarList", "https://perryma.tk/arowe01/calendar.html", "classProjectsList");
     
     
     
